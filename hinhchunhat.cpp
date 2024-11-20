34d76f7b961de399c74a563fcef46a23efba95e1
khanhh89 <khanhabcabc0000@gmail.com>
2024-11-20 14:49:30 +0700

    Add files via upload

---
+++ hinhchunhat.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+
+
+float a = 10;
+float b = 20;
+
+float chuvi = 2 * (a + b);
+float dientich = a * b;  
+
+printf("chieu dai hinh chu nhat la: %f\n", a);
+printf("chieu rong hinh chu nhat la: %f\n", b);
+printf("chu vi hinh chu nhat la: %f\n", chuvi);
+printf("dien tich hinh chu nhat la: %f\n", dientich);
+}
+
