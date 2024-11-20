362c5d5c083f5cb69be24d74b81d524aede0cd45
khanhh89 <khanhabcabc0000@gmail.com>
2024-11-20 14:17:04 +0700

    Add files via upload

---
+++ bt1.cpp
@@ -0,0 +1,31 @@
+#include <windows.h>
+
+int main(){
+	//int dung de luu tru so nguyen
+	int age=10;
+	// khoi tao bien"age" bang 10
+	
+	float height=1.2;
+	//dung de luu tru so thuc
+	
+	double pi = 3.14159265;
+	//lay gia tri gan dung cua bien pi
+	
+	char phan = 'a';
+	
+	// khoi tao bien Anh voi giatri bang A
+	
+	bool school = 1;
+	// co gia tri dung hoac sai (T,F)
+	
+	long number = 5568; 
+	// khoi tao so nguyen lon hon kieu int
+	
+	
+
+	
+	
+	
+	
+	
+}
