c22389d83c2388f9b40ca402358d84363df95a55
khanhh89 <khanhabcabc0000@gmail.com>
2024-11-20 14:29:00 +0700

    Add files via upload

---
+++ chuvidthv.cpp
@@ -0,0 +1,23 @@
+
+#include <stdio.h>
+
+int main(){
+	//khai bao canh hinh vuong
+	float side = 10;
+	
+	// tinh toan
+	
+	float chuvi = side * 4;
+	float dientich = side * side;
+	
+	// in ket qua ra man hinh
+	
+	printf("canh hinh vuong la: %.2f\n", side);
+	printf("chu vi hinh vuong la: %.2f\n", chuvi);
+	printf("dien tich hinh vuong la: %.2f\n", dientich);
+	
+	return 0;
+}
+
+
+
