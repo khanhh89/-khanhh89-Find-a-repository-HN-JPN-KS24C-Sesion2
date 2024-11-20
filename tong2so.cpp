5ce3d3baeea779a7eda130349605e43a1e222279
khanhh89 <khanhabcabc0000@gmail.com>
2024-11-20 13:22:38 +0700

    Add files via upload

---
+++ tong2so.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+	int main(){
+		//khai bao kieu so thuc
+		float a, b, tong;
+	
+		printf("nhap a=");
+		scanf("%f", &a);
+		printf("nhap b=");
+		scanf("%f", &b);
+		
+		//luu tru vao bien tong
+		
+		 tong = a + b;
+		 
+		//in ket qua ra man hinh
+		
+		printf("tong=   %f", tong);
+		
+
+	  
+	  }
+
+
